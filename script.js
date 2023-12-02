969ac28d42987ccaaae2f091f2a602e60cf7be09
Kev-rod43 <kev.rod43@yahoo.com>
2023-12-01 20:25:24 -0500

    finished app functionality, displayed date on the header, imported advancedFormat plugin for dayjs

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-for (i=0;i<9;i++){
-  let idsForHours = [
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-  ];
-  let workDayHours = [
-    "9AM",
-    "10AM",
-    "11am",
-    "12PM",
-    "1PM",
-    "2PM",
-    "3PM",
-    "4PM",
-    "5PM",
-  ]
-  let hourDivTag = $("<div class='row time-block '><div class='col-2 col-md-1 hour text-center py-3'>9AM</div><textarea class='col-8 col-md-10 description' rows='3'> </textarea><button class='btn saveBtn col-2 col-md-1' aria-label='save'><i class='fas fa-save' aria-hidden='true'></i></button></div>");
-  hourDivTag.attr("id", idsForHours[i]);
-  hourDivTag.children().eq(0).text(workDayHours[i]);
-  $(".container-lg").append(hourDivTag);
+//initializes the object that stores the hour notes, then pulls the properties from localstorage if it exists
+let savedHourNotes = new Object;
+if (JSON.parse(localStorage.getItem("saved-hour-notes"))!==null){
+  savedHourNotes= JSON.parse(localStorage.getItem("saved-hour-notes"));
 }
 
+// displays the hour notes text that was saved locally
+$("textarea").each(function(){
+  let timeBlocktIndex = $(this).parent().attr("data-element-index")
+  let savedNoteForHour = savedHourNotes[timeBlocktIndex]
+  if (savedNoteForHour !== undefined){
+    $(this).val(savedHourNotes[timeBlocktIndex])
+  }
+}
+);
+
+//sets the text of the header to todays date
+$("#currentDay").text(dayjs().format("dddd, MMMM Do"))
+
+//sets the style utility classes that sets the background color for the time blocks depending on wether they are in the present, past or future
 $(".time-block").each(function(){
   let currentHour = dayjs().hour();
-  console.log($(this).attr("id"))
-  if ($(this).attr("id") === currentHour){
+  if ($(this).attr("data-element-hour") === currentHour){
     $(this).addClass("present")
-  } else if ($(this).attr("id") < currentHour){
+  } else if ($(this).attr("data-element-hour") < currentHour){
     $(this).addClass("past")
   } else {
     $(this).addClass("future")
@@ -43,21 +30,14 @@ $(".time-block").each(function(){
 }
 );
 
+//saves the hour notes to the savedHourNotes object, then stores that object in local data
+$(".saveBtn").on("click", function(event){
+  let timeBlocktIndex = $(event.currentTarget).parent().attr("data-element-index");
+  let hourNotes = $(event.currentTarget).siblings("textarea").val();
 
+  savedHourNotes[timeBlocktIndex] = hourNotes;
 
-//$("saveBtn").on("click", function(){
+  localStorage.setItem("saved-hour-notes", JSON.stringify(savedHourNotes));
+}
+)
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
